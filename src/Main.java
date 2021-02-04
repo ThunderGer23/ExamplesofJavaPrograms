350ea3c05e1c24977ffb8be22120b2bba9e70228
ThunderGer23 <Cuando2son1@hotmail.com>
2021-02-04 02:01:50 -0600

    add documentation for best performance.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+// Librerias para el correcto funcionamiento del programa
 import java.util.Scanner;
+//Clases creadas para los ejemplos en la carpeta de Codes
 import Codes.*;
+//Códigos para el flujo correcto de la aplicación
+import Codes.Validaciones.validareturn;
 
+//Clase principal
 public class Main {
     public static void main(String[] args) {
+        //Declaración de objetos y variables de control para el flujo del programa
         Scanner input = new Scanner(System.in);
+        validareturn V1 = new validareturn();
         char ValRef = '0';
+
+        //Bucle de menu
         do {
             if (ValRef == '0') {
                 System.out.println("Ingresa una opción" +
@@ -16,37 +25,46 @@ public class Main {
                         "3.Calcular el factorial de un numero\n" +
                         "4.Comprobar si el numero es positivo o" +
                         "negativo \n" +
-                        "5.Multiplicación de 2 numeros");
+                        "5.Multiplicación de 2 numeros\n" +
+                        "6.Multiplicacion de n numeros");
 
                 ValRef = input.nextLine().charAt(0);
                 switch (ValRef) {
                     case '1':
+                        //ejemplo1
                         example1 E1 = new example1();
                         E1.runExample();
-                        System.out.println("Enter para vovler al menu...\n" +
-                                "'E' para salir");
-                        ValRef = input.nextLine().charAt(0);
                         break;
                     case '2':
+                        //Ejemplo2
                         example2 E2 = new example2();
                         E2.runExample();
                         break;
                     case '3':
+                        //Ejemplo3
                         example3 E3 = new example3();
                         E3.runExample();
                         break;
                     case '4':
+                        //Ejemplo4
                         example4 E4 = new example4();
                         E4.runExample();
                         break;
                     case '5':
+                        //Ejemplo5
                         example5 E5 = new example5();
                         E5.runExample();
+                        break;
+                    case '6':
+                        //Ejemplo6
+                        example6 E6 = new example6();
+                        E6.runExample();
                         break;
                     default:
                         System.out.println("Dato no valido para el sistema");
                         break;
                 }
+                ValRef = V1.valida();
             }
         }while(ValRef != 'E');
     }
